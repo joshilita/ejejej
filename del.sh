0bd12579a4b96b71a840f317090a2b887d42de02
joshilita <52619112+joshilita@users.noreply.github.com>
2019-07-07 02:30:44 -0400

    Add files via upload

---
+++ del.sh
@@ -0,0 +1,23 @@
+#!/system/bin/sh
+
+echo Deleting YAY
+
+rm -rf YAY
+
+echo Done!
+
+cd /sdcard
+
+cd YAY
+
+echo please wait while we clear
+
+wait 
+
+wait
+
+wait
+
+clear
+
+echo done!
---
+++ no.sh
@@ -0,0 +1,13 @@
+cd /sdcard
+
+echo subscribe
+#!/system/bin/sh
+
+echo to
+#!/system/bin/sh
+
+echo joshilita
+
+echo creating YAY...
+
+mkdir YAY
